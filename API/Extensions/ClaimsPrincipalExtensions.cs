dd9c29054ab1820cc63ce0133923e4f0bd2e34e4
rick-guiz <rick.guiz@gmail.com>
2020-10-06 19:04:40 +0800

    End of section 18

---
+++ API/Extensions/ClaimsPrincipalExtensions.cs
@@ -0,0 +1,14 @@
+using System.Security.Claims;
+using System.Linq;
+
+namespace API.Extensions
+{
+    public static class ClaimsPrincipalExtensions
+    {
+        public static string RetrieveEmailFromPrincipal(this ClaimsPrincipal user)
+        {
+            return user?.Claims?.FirstOrDefault (x => x.Type == ClaimTypes.Email)?.Value;
+        }
+
+    }
+}
